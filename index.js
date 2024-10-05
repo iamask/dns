e375da0f702b23c5a3f68776468c7de83c5fc602
Ajay <iamask@live.com>
2024-10-05 22:17:59 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+export default {
+  async fetch(request, env, ctx) {
+    const domain = "www.cloudflare.com";
+
+    const dnsQueryUrl = `https://cloudflare-dns.com/dns-query?name=${domain}&type=A`;
+
+    const dns = await fetch(dnsQueryUrl, {
+      headers: { "accept": "application/dns-json" },
+      method: "GET"
+    });
+
+    const dnsdata = await dns.json();
+
+    console.log(dnsdata);
+
+    // Wrap the JSON data in a Response object
+    return new Response(JSON.stringify(dnsdata), {
+      headers: { "Content-Type": "application/json" }
+    });
+  },
+};
